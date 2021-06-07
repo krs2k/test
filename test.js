6db8503b740dd128ef2c7043bbc5a80f6e0d6716
krs2k <k.kamasa@gmail.com>
2021-06-07 10:38:17 +0200

    asdfsdfs

--- test.js
+++ test.js
@@ -2,16 +2,5 @@ const AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    try {
-        let data = await ddb.put({
-            TableName: "users",
-            Item: {
-                id: "test"
-            }
-        }).promise();
-
-    } catch (err) {
-        // error handling goes here
-    };
     return { "message": "Successfully executed" };
 };
